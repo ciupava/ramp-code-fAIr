7fee0ed584fcf7d93f558550ace299c21e9d60e2
ciupava <ciupava@gmail.com>
2024-05-14 19:35:02 +0000

    more tests on F1 score

--- ramp/training/metric_constructors.py
+++ ramp/training/metric_constructors.py
@@ -7,6 +7,9 @@
 
 import tensorflow as tf
 import tensorflow_addons as tfa
+# import keras
+# from tf.python import keras
+# from keras.src import ops
 
 # adding logging
 import logging
@@ -15,6 +18,27 @@ import functools
 log = logging.getLogger()
 log.addHandler(logging.NullHandler())
 
+class F1_Score(tf.keras.metrics.Metric):
+    #  from https://stackoverflow.com/a/64477522
+    def __init__(self, class_id, name='f1_score', **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.f1 = self.add_weight(name='f1', initializer='zeros')
+        self.precision_fn = tf.keras.metrics.Precision(class_id=class_id)
+        self.recall_fn = tf.keras.metrics.Recall(class_id=class_id)
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        p = self.precision_fn(y_true, y_pred)
+        r = self.recall_fn(y_true, y_pred)
+        self.f1.assign(tf.math.divide_no_nan(2 * (p * r), (p + r)))
+
+    def result(self):
+        return self.f1
+
+    def reset_states(self):
+        # we also need to reset the state of the precision and recall objects
+        self.precision_fn.reset_states()
+        self.recall_fn.reset_states()
+        self.f1.assign(0)
 
 def get_sparse_categorical_accuracy_fn(cfg):
     return tf.keras.metrics.SparseCategoricalAccuracy()
@@ -67,13 +91,28 @@ def get_recall_fn(cfg):
         # thresholds=None,top_k=None, class_id=None, name=None, dtype=None
     )
 
+
 def get_f1_score_fn(cfg):
+    return F1_Score(class_id=1)
+    # prec = get_precision_fn(cfg)
+    # rec = get_recall_fn(cfg)
+    # precision = ops.convert_to_tensor(prec)
+    # recall = ops.convert_to_tensor(rec)
+    # num_value = 2 *precision * recall
+    # den_value = precision + recall
+    # f1_score = ops.divide(num_value, den_value)
+    # return f1_score
     # ---
-    return tfa.metrics.F1Score(
-        num_classes=4,
-        average='macro',
-        threshold=0.5,
-        name="f1score"
+    # p = get_precision_fn(cfg)
+    # r = get_recall_fn(cfg)
+    # f1 = (2 * (p * r))/(p + r)
+    # return f1
+    # ---
+    # return tfa.metrics.F1Score(
+    #     num_classes=1,
+    #     # average=None,
+    #     threshold=None,
+    #     name="f1score"
     # ---
     # return tf.keras.metrics.F1Score(
     #     name="f1score",
@@ -83,7 +122,8 @@ def get_f1_score_fn(cfg):
     # precision = precision_m(y_true, y_pred)
     # recall = recall_m(y_true, y_pred)
     # return 2*((precision*recall)/(precision+recall+K.epsilon()))
-    )
+    # )
+
 
 #---
 
